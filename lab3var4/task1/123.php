0be7b989e00cd80816137b3ce527574db233beaa
Illya Kohanchuk <55888815+Krasav4ik66@users.noreply.github.com>
2020-03-19 21:17:24 +0200

    with table

--- lab3var4/task1/123.php
+++ lab3var4/task1/123.php
@@ -1,13 +1,17 @@
+
 <?php
 	//print_r($_POST);
 	 $str = $_POST['array'];
-	  $arr = explode( ",",$str );
-	  for ( $i=0; $i < count($arr); $i++ ){
-	    if( $arr[$i] == 0 ){
-            unset( $arr[$i] );
+	  $startarr = explode( ",",$str );
+	  $k = 0;
+	  $arr = array();
+	  for ( $i=0; $i < count($startarr); $i++ ){
+	    if( $startarr[$i] == 0 ){
+            continue;
 	    }
 	    else{
-	        $arr[$i] = (int)$arr[$i];
+	        $arr[$k] = (int)$startarr[$i];
+	        $k++;
 	    }
 	  }
 	  if( count($arr) == 0 ){
@@ -15,7 +19,12 @@
 	    exit;
 	  }
 	  if( count($arr) == 1 ){
-      	    print_r($arr );
+	        
+      	    echo '<table style = " border-collapse: collapse; ">';
+      	    echo '<tr>';
+      	    echo '<td style = "border: 1px solid grey; padding: 10px; font-size: 40px;">'.$arr[0].'</td>';
+      	     echo '</tr>';
+      	     echo '</table>';
       	    exit;
       	  }
 	  for ( $i = 2; $i <= count($arr); $i++ ){
@@ -29,8 +38,18 @@
 	    $finalArr[$i] = array();
 	    for ($j=0; $j < count($arr) / $kol; $j++) {
 	        $finalArr[$i][$j] = $arr[$k];
+	        $k++;
 	    }
-	    $k++;
+
 	  }
-	  print_r($finalArr);
+	  echo '<table style = " border-collapse: collapse; ">';
+      	  for ($i=0; $i < $kol; $i++) {
+      	        echo '<tr>';
+            	    for ($j=0; $j < count($arr) / $kol; $j++) {
+            	        echo '<td style = "border: 1px solid grey; padding: 10px; font-size: 40px;">'.$finalArr[$i][$j].'</td>';
+            	        //echo $j;
+            	    }
+      	        echo '</tr>';
+      	  }
+      	  echo '</table>';
 ?>
